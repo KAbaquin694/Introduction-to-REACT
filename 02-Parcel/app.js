68d4bb743ffdd1635d925402d684fb960e584e7e
Khalel <KAbaquin@west-mec.org>
2019-11-21 14:36:25 -0700

    Update 2
    added components folder with a Navbar.js, Map.js, Footer.js file & added code

--- 02-Parcel/app.js
+++ 02-Parcel/app.js
@@ -1,1 +1,5 @@
-import 
+// This file has all of the COMPONENTS of our Web APP
+import React from 'react'
+import Navbar from './components/Navbar';
+// The App component
+export default () => ( <Navbar /> )
